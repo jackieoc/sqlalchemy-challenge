7f475614ed37dbb47ec42fadfc06825400256697
Jackie <jackie.ochuida@gmail.com>
2023-09-05 15:25:37 -0400

    app started

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+from datetime import timedelta
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, desc
+
+from flask import Flask, jsonify
 
 
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
 # reflect an existing database into a new model
+
 
 # reflect the tables
 
